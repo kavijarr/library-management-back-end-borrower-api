0bb57d1d49cdc05169799d446de13936a3e0dd53
kavijarr <kavijakumuditha12@gmail.com>
2024-02-23 14:04:00 +0530

    first commit

---
+++ src/main/java/edu/icet/entity/UserEntity.java
@@ -0,0 +1,12 @@
+package edu.icet.entity;
+
+public class UserEntity {
+    Long id;
+    String firstName;
+    String lastName;
+    String userName;
+    String email;
+    String address;
+    
+
+}
